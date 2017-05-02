e602d3b150662b8a985dfa300724c1161076f32d
qinwf <qinwf@rweekly.org>
2017-05-02 23:50:15 +0800

    update travis ci to enable non-master branches

--- _deploy.sh
+++ _deploy.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "rforwards-auto@protonmail.com"
 git config --global user.name "rforwards-auto"
 
