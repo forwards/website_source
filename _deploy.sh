88f2923310eee7df313368d3a40b723dbfb68cd6
qinwf <qinwf@rweekly.org>
2017-05-15 00:04:41 +0800

    echo message about which build is running on travis

--- _deploy.sh
+++ _deploy.sh
@@ -13,8 +13,8 @@ touch .nojekyll
 git add --all *
 git commit -m "Update website" || true
 
-[ "$TRAVIS_PULL_REQUEST" != "false" ] && git push --force --quiet "https://$GITHUB_PAT@github.com/rforwards-auto/rforwards-auto.github.io.git" master > /dev/null 2>&1 && exit 0
+[ "$TRAVIS_PULL_REQUEST" != "false" ] && echo push pull request build result && git push --force --quiet "https://$GITHUB_PAT@github.com/rforwards-auto/rforwards-auto.github.io.git" master > /dev/null 2>&1 && exit 0
 
-[ "${TRAVIS_BRANCH}" != "master" ] && git push --force --quiet "https://$GITHUB_PAT@github.com/rforwards-auto/rforwards-auto.github.io.git" master > /dev/null 2>&1 && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && echo push non-master branch build result && git push --force --quiet "https://$GITHUB_PAT@github.com/rforwards-auto/rforwards-auto.github.io.git" master > /dev/null 2>&1 && exit 0
 
-git push --force --quiet "https://$GITHUB_PAT@github.com/forwards/forwards.github.io" master > /dev/null 2>&1
+[ "${TRAVIS_BRANCH}" = "master" ] && echo push forwards.github.io build result && git push --force --quiet "https://$GITHUB_PAT@github.com/forwards/forwards.github.io" master > /dev/null 2>&1
