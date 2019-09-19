3edaaf8870784a1193930fc0d0d9ca991b70f973
Heather Turner <ht@heatherturner.net>
2019-09-19 11:15:41 +0100

    do only one build for PRs

--- _deploy.sh
+++ _deploy.sh
@@ -24,6 +24,6 @@ git commit -m "Update website" || true
 
 [ "$TRAVIS_PULL_REQUEST" != "false" ] && echo push pull request build result && git push --force --quiet "https://$GITHUB_PAT@github.com/rforwards-auto/rforwards-auto.github.io.git" master > /dev/null 2>&1 && exit 0
 
-[ "${TRAVIS_BRANCH}" != "master" ] && echo push non-master branch build result && git push --force --quiet "https://$GITHUB_PAT@github.com/rforwards-auto/rforwards-auto.github.io.git" master > /dev/null 2>&1 && exit 0
+[[ (${TRAVIS_BRANCH} != master && ${TRAVIS_PULL_REQUEST} = false) ]] && echo push non-master branch build result && git push --force --quiet "https://$GITHUB_PAT@github.com/rforwards-auto/rforwards-auto.github.io.git" master > /dev/null 2>&1 && exit 0
 
 [ "${TRAVIS_BRANCH}" = "master" ] && echo push forwards.github.io build result && git push --force --quiet "https://$GITHUB_PAT@github.com/forwards/forwards.github.io" master > /dev/null 2>&1
